4a1e2290e0eb6a3013122d573c8cd10fef39450e
totsumaru <techstart35@gmail.com>
2023-11-25 01:19:02 +0900

    ポイントに応じた画像を表示

--- bot/handler/interaction_craete/send_point.go
+++ bot/handler/interaction_craete/send_point.go
@@ -2,6 +2,7 @@ package interaction_craete
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/totsumaru/gacha-bot-backend/lib/errors"
@@ -20,12 +21,43 @@ func SendPoint(s *discordgo.Session, i *discordgo.InteractionCreate, currentPoin
 **%d** pt
 `
 
+	imageURL := fmt.Sprintf(
+		"https://gacha-bot-image-generate.vercel.app/api/card?username=%s&avatar=%s&point=%d",
+		url.QueryEscape(i.Member.User.Username),
+		url.QueryEscape(i.Member.User.AvatarURL("")),
+		currentPoint,
+	)
+
+	// MEMO: // URLではなくて、画像データで送りたい時はここを使用。
+	// 現状はEmbedに納めたいのでコメントアウトしている。
+	//
+	//// 画像URLからデータを取得
+	//httpRes, err := http.Get(imageURL)
+	//if err != nil {
+	//	return errors.NewError("画像を取得できません", err)
+	//}
+	//defer httpRes.Body.Close()
+	//
+	//imageData, err := io.ReadAll(httpRes.Body)
+	//if err != nil {
+	//	return errors.NewError("画像データの読み取り中にエラーが発生しました", err)
+	//}
+	//
+	//imageReader := bytes.NewReader(imageData)
+
 	embed := &discordgo.MessageEmbed{
 		Description: fmt.Sprintf(description, currentPoint),
+		Image: &discordgo.MessageEmbedImage{
+			URL: imageURL,
+		},
 	}
 
 	webhook := &discordgo.WebhookEdit{
 		Embeds: &[]*discordgo.MessageEmbed{embed},
+		//Files: []*discordgo.File{{
+		//	Name:   "image.jpg",
+		//	Reader: imageReader,
+		//}},
 	}
 	if _, err = editFunc(i.Interaction, webhook); err != nil {
 		return errors.NewError("レスポンスを送信できません", err)
