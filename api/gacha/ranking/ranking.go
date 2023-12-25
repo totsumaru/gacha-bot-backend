4233707dc532c4e260440fb84e22c60bc3d4fec9
totsumaru <techstart35@gmail.com>
2023-12-25 17:13:15 +0900

    discordの取得をgoroutineに変更

--- api/gacha/ranking/ranking.go
+++ api/gacha/ranking/ranking.go
@@ -3,6 +3,7 @@ package ranking
 import (
 	"log"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -59,28 +60,64 @@ func GetRanking(e *gin.Engine, db *gorm.DB) {
 }
 
 // ドメインのユーザーデータをAPIのレスポンスに変換します
-func ConvertToAPIGachaRes(
-	s *discordgo.Session,
-	datas []domainUserData.UserData,
-) ([]ResUserData, error) {
-	res := make([]ResUserData, 0)
-
-	for i, userData := range datas {
-		userID := userData.UserID().String()
-		u, err := s.User(userID)
-		if err != nil {
-			return nil, errors.NewError("ユーザー名を取得できません", err)
-		}
-
-		resUserData := ResUserData{
-			UserName:  u.Username,
-			AvatarURL: u.AvatarURL(""),
-			Point:     userData.Point().Int(),
-			Rank:      i + 1,
-		}
-
-		res = append(res, resUserData)
+func ConvertToAPIGachaRes(s *discordgo.Session, datas []domainUserData.UserData) ([]ResUserData, error) {
+	//res := make([]ResUserData, 0)
+	//
+	//for i, userData := range datas {
+	//	userID := userData.UserID().String()
+	//	u, err := s.User(userID)
+	//	if err != nil {
+	//		return nil, errors.NewError("ユーザー名を取得できません", err)
+	//	}
+	//
+	//	resUserData := ResUserData{
+	//		UserName:  u.Username,
+	//		AvatarURL: u.AvatarURL(""),
+	//		Point:     userData.Point().Int(),
+	//		Rank:      i + 1,
+	//	}
+	//
+	//	res = append(res, resUserData)
+	//}
+	type result struct {
+		data ResUserData
+		err  error
 	}
+
+	results := make(chan result, len(datas))
+	for i, userData := range datas {
+		go func(i int, userData domainUserData.UserData) {
+			userID := userData.UserID().String()
+			u, err := s.User(userID)
+			if err != nil {
+				results <- result{err: errors.NewError("ユーザー名を取得できません", err)}
+				return
+			}
+
+			resUserData := ResUserData{
+				UserName:  u.Username,
+				AvatarURL: u.AvatarURL(""),
+				Point:     userData.Point().Int(),
+				Rank:      i + 1,
+			}
+
+			results <- result{data: resUserData}
+		}(i, userData)
+	}
+
+	res := make([]ResUserData, 0, len(datas))
+	for range datas {
+		result := <-results
+		if result.err != nil {
+			return nil, result.err
+		}
+		res = append(res, result.data)
+	}
+
+	// ポイントで降順にソート
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Point > res[j].Point
+	})
 
 	return res, nil
 }
